5ed53eb5a820d2aa19fb0406dd7b6c4ef6d2a665
Aravind Rajesh Kanna <aravind.r@techmango.net>
2021-09-24 10:23:38 +0100

    Week 1 task

---
+++ week1_pkg/q1.py
@@ -0,0 +1,27 @@
+# -------------------------------------------
+# Test the following statements in PyCharm
+# -------------------------------------------
+
+
+print("\t"*2+"Title")
+print("Hello")
+print("Hello World")
+a = 5
+b = 12.3456789
+c = a+b
+# d = "A number " + b
+# Here the mistake is concatenating the string with float value
+# Correction is we have to convert the float type to string
+d = "A number " + str(b)
+e = a +\
+    b +\
+    c
+f = 2
+g = a/f
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
