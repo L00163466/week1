d0cd107728d2f8469bec8ed44b190374cdc1c531
Aravind Rajesh Kanna <aravind.r@techmango.net>
2021-09-24 10:53:42 +0100

    question 2 done

---
+++ week1_pkg/q2.py
@@ -0,0 +1,25 @@
+# -------------------------------------------
+# Test the following statements in PyCharm
+
+# Available under GNU public license
+# -------------------------------------------
+
+
+top_icon = "^"
+left_icon = "<"
+bottom_icon = "v"
+right_icon = ">"
+
+print(top_icon*50)
+print(left_icon + " "*48 + right_icon)
+print(left_icon + " "*48 + right_icon)
+print(left_icon + " "*48 + right_icon)
+print(left_icon + " "*21 + "Title" + " "*22 + right_icon)
+print(left_icon + " "*48 + right_icon)
+print(left_icon + " "*48 + right_icon)
+print(left_icon + " "*9 + "A Option a" + " "*29 + right_icon)
+print(left_icon + " "*9 + "B Option b" + " "*29 + right_icon)
+print(left_icon + " "*9 + "C Option c" + " "*29 + right_icon)
+print(left_icon + " "*48 + right_icon)
+print(left_icon + " "*48 + right_icon)
+print(bottom_icon*50)
